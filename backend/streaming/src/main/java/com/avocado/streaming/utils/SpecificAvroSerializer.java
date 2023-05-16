8fe2cbf0cca159b594a69da90810d27cf5619cc4
인덕 <indeokiya@snu.ac.kr>
2023-05-17 02:46:00 +0900

    S08P31A506-159-streaming [feat] click, payment event 전처리 후 통계 서버로 전달

--- backend/streaming/src/main/java/com/avocado/streaming/utils/SpecificAvroSerializer.java
+++ backend/streaming/src/main/java/com/avocado/streaming/utils/SpecificAvroSerializer.java
@@ -1,54 +1,57 @@
 //package com.avocado.streaming.utils;
 //
-//import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
-//import io.confluent.kafka.serializers.KafkaAvroSerializer;
-//import org.apache.avro.generic.GenericRecord;
 //import org.apache.kafka.common.annotation.InterfaceStability;
 //import org.apache.kafka.common.header.Headers;
 //import org.apache.kafka.common.serialization.Serializer;
 //
 //import java.util.Map;
 //
+//import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+//import io.confluent.kafka.serializers.KafkaAvroSerializer;
+//
 ///**
-// * A schema-registry aware serializer for writing data in "generic Avro" format.
+// * A schema-registry aware serializer for writing data in "specific Avro" format.
 // *
 // * <p>This serializer writes data in the wire format defined at
 // * http://docs.confluent.io/current/schema-registry/docs/serializer-formatter.html#wire-format.
 // * It requires access to a Confluent Schema Registry endpoint, which you must
-// * {@link GenericAvroSerializer#configure(Map, boolean)} via the parameter
+// * {@link SpecificAvroSerializer#configure(Map, boolean)} via the parameter
 // * "schema.registry.url".</p>
 // *
-// * <p>See {@link GenericAvroDeserializer} for its deserializer counterpart.</p>
+// * <p>See {@link SpecificAvroDeserializer} for its deserializer counterpart.</p>
 // */
 //@InterfaceStability.Unstable
-//public class GenericAvroSerializer implements Serializer<GenericRecord> {
+//public class SpecificAvroSerializer<T extends org.apache.avro.specific.SpecificRecord>
+//        implements Serializer<T> {
 //
 //    private final KafkaAvroSerializer inner;
 //
-//    public GenericAvroSerializer() {
+//    public SpecificAvroSerializer() {
 //        inner = new KafkaAvroSerializer();
 //    }
 //
 //    /**
 //     * For testing purposes only.
 //     */
-//    GenericAvroSerializer(final SchemaRegistryClient client) {
+//    SpecificAvroSerializer(final SchemaRegistryClient client) {
 //        inner = new KafkaAvroSerializer(client);
 //    }
 //
 //    @Override
 //    public void configure(final Map<String, ?> serializerConfig,
 //                          final boolean isSerializerForRecordKeys) {
-//        inner.configure(serializerConfig, isSerializerForRecordKeys);
+//        inner.configure(
+//                ConfigurationUtils.withSpecificAvroEnabled(serializerConfig),
+//                isSerializerForRecordKeys);
 //    }
 //
 //    @Override
-//    public byte[] serialize(final String topic, final GenericRecord record) {
+//    public byte[] serialize(final String topic, final T record) {
 //        return serialize(topic, null, record);
 //    }
 //
 //    @Override
-//    public byte[] serialize(final String topic, final Headers headers, final GenericRecord record) {
+//    public byte[] serialize(final String topic, final Headers headers, final T record) {
 //        return inner.serialize(topic, headers, record);
 //    }
 //
