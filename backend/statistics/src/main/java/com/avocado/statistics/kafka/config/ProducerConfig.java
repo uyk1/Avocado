929f2128d934dc1a71c249c0872edcfd91562800
인덕 <indeokiya@snu.ac.kr>
2023-05-17 21:40:18 +0900

    S08P31A506-159-streaming [feat] 회원 이벤트 및 광고 정보 이벤트 구현 중

---
+++ backend/statistics/src/main/java/com/avocado/statistics/kafka/config/ProducerConfig.java
@@ -0,0 +1,43 @@
+package com.avocado.statistics.kafka.config;
+
+import com.avocado.AdStatus;
+import com.avocado.statistics.db.mysql.entity.jpa.Click;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerializer;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ProducerConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+
+
+
+    @Bean
+    public KafkaTemplate<Integer, AdStatus> AdStatusKafkaTemplate() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+        props.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put("schema.registry.url", "http://a5ef82e13fcbc44689f93c4924981608-494875664.ap-northeast-2.elb.amazonaws.com:8081");
+//        props.put("specific.avro.reader", "true");
+
+        ProducerFactory<Integer, AdStatus> adStatusProducerFactory = new DefaultKafkaProducerFactory<>(props);
+
+        return new KafkaTemplate<>(adStatusProducerFactory);
+    }
+
+
+
+
+}
